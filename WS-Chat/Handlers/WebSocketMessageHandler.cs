705158c1a5c7981c4fbb33ace83ca36ced24d934
Abdullah Ramzan <khawajaabdullah2000@gmail.com>
2021-10-06 16:51:04 +0500

    Add project files.

---
+++ WS-Chat/Handlers/WebSocketMessageHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using WS_Chat.SocketManager;
+
+namespace WS_Chat.Handlers
+{
+    public class WebSocketMessageHandler : SocketHandler
+    {
+        public WebSocketMessageHandler(ConnectionManager connections) : base(connections)
+        {
+        }
+
+        public override Task Receive(WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
